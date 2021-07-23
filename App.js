aca31ff6c8c8161281e08001b32f48d71c104e8c
Samba Diaw <sambachimerediaw@gmail.com>
2021-07-23 19:36:46 +0200

    wip

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from "expo-constants";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import AuthorRow from "./components/AuthorRow";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AuthorRow 
+          fullname={'First Last'}
+          linkText={'Comments'}
+          onPressLinkText={() => {
+            console.log('Pressed link!');
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
