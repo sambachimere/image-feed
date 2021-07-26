d8bb6c7ebc5547f1f91f49827f0140dc501bf02d
Samba Diaw <sambachimerediaw@gmail.com>
2021-07-26 17:40:23 +0200

    app is finished

--- App.js
+++ App.js
@@ -1,29 +1,102 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Modal, 
+  Platform, 
+  StyleSheet, 
+  View, 
+} from 'react-native';
 import Constants from "expo-constants";
 
-import AuthorRow from "./components/AuthorRow";
+import Feed from './screens/Feed';
+import Comments from "./screens/Comments"
+
+const items = [
+  {id: 0, author: 'Kemi Seba'},
+  {id: 1, author: 'Samba Diaw'},
+];
 
 export default class App extends React.Component {
+  state = {
+    commentsForItem: {},
+    showModal: false,
+    selectedItemId: null,
+  };
+
+  openCommentScreen = id => {
+    this.setState({
+      showModal: true,
+      selectedItemId: id,
+    });
+  };
+
+  closeCommentScreen = () => {
+    this.setState({
+      showModal: false,
+      selectedItemId: null,
+    });
+  };
+
+  onSubmitComment = (text) => {
+    const { selectedItemId, commentsForItem } = this.state;
+    const comments = commentsForItem[selectedItemId] || [];
+
+    const updated = {
+      ...commentsForItem,
+      [selectedItemId]: [...comments, text],
+    };
+
+    this.setState({ commentsForItem: updated });
+  };
+
   render() {
+    const { commentsForItem, showModal, selectedItemId } = this.state;
+
     return (
       <View style={styles.container}>
-        <AuthorRow 
-          fullname={'First Last'}
-          linkText={'Comments'}
-          onPressLinkText={() => {
-            console.log('Pressed link!');
-          }}
+        <Feed 
+          style={styles.feed}
+          commentsForItem={commentsForItem} 
+          onPressComments={this.openCommentScreen} 
         />
+        <Modal 
+          visible={showModal}
+          animationType="slide"
+          onRequestClose={this.closeCommentScreen}
+        >
+          <Comments 
+            style={styles.comments}
+            comments={commentsForItem[selectedItemId] || []}
+            onClose={this.closeCommentScreen}
+            onSubmitComment={this.onSubmitComment}
+          />
+        </Modal>
       </View>
     );
   }
 }
 
+const platformVersion =
+  Platform.OS === "ios"
+    ? parseInt(Platform.Version, 10)
+    : Platform.Version;
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
+  },
+  feed: {
+    flex: 1,
+    marginTop:
+      Platform.OS === "android" || platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0,
+  },
+  comments: {
+    flex: 1,
+    marginTop: 
+      Platform.OS = "ios" && platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0,
   },
 });
